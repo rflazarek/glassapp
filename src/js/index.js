62170a5ba723b2459ebdfdbfacf248e62051d8f1
Rafal Lazarek <rflazarek@gmail.com>
2022-03-18 22:01:19 +0100

    JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,39 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
-/* place your code below */
+const glassCounter = document.querySelector(".glass__counter--js");
+const buttonAdd = document.querySelector(".button-add--js");
+const buttonRemove = document.querySelector(".button-remove--js");
+const key = new Date().toLocaleString().slice(0, 10);
 
-console.log('HELLO 🚀')
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue){
+  currentGlassCounter=localStorageValue;
+}
+  else{
+    localStorage.setItem(key, 0);
+  }
+
+glassCounter.innerHTML=currentGlassCounter;
+
+buttonAdd.addEventListener('click', () => {
+  currentGlassCounter=parseInt(currentGlassCounter)+1;
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
+
+buttonRemove.addEventListener('click', () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
+
